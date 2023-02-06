46794571260891ae7bd3c7b6f747684d8bb67072
danipetro21 <idany346@gmail.com>
2023-02-06 17:04:23 +0100

    scafoling | maincontroller | model

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use app\Models\Comic;
+
+class MainController extends Controller
+{
+    //
+    //--- INDEX
+
+    public function home() {
+
+        $comics = Comic::orderBy('created_at', 'DESC') -> get();
+
+        return view('pages.home', compact('comics'));
+    }
+}
